831293c010864da356ad6b7959ab256f6eb7c8e0
jennysjottings <jenny@jennysjottings.co.uk>
2021-12-19 09:33:52 +0000

    RED: speedy shipping

--- CourierKataTests/OrderCostCalculatorTests.cs
+++ CourierKataTests/OrderCostCalculatorTests.cs
@@ -30,5 +30,29 @@ namespace CourierKataTests
             Assert.AreEqual(3, result.Items.First().Cost);
             Assert.AreEqual("Small Parcel", result.Items.First().Description);
         }
+
+        [Test]
+        public void SpeedyShippingShouldBeIncludedAsItem()
+        {
+            var calculator = new OrderCostCalculator();
+
+            var result = calculator.CalculateCost(1, 1, 1, true);
+
+            Assert.AreEqual(2, result.Items.Count());
+        }
+
+        [TestCase(1, 1, 1, 3)]
+        [TestCase(1, 5, 5, 8)]
+        [TestCase(4, 4, 4, 15)]
+        [TestCase(10, 4, 4, 25)]
+        public void SpeedyShippingShouldDoubleCostOfOrderWithoutAffectingOtherItems(int width, int length, int height, decimal cost)
+        {
+            var calculator = new OrderCostCalculator();
+
+            var result = calculator.CalculateCost(width, length, height, true);
+
+            Assert.AreEqual(cost * 2, result.TotalCost);
+            Assert.AreEqual(cost, result.Items.Single(x => !x.Description.ToLower().Equals("speedy shipping")).Cost);
+        }
     }
 }
