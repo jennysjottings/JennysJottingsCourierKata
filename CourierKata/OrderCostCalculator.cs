413640c3181d4873ac82a0775a916f0fa495cc6f
jennysjottings <jenny@jennysjottings.co.uk>
2021-12-19 09:16:36 +0000

    GREEN: output should contain parcel category

--- CourierKata/OrderCostCalculator.cs
+++ CourierKata/OrderCostCalculator.cs
@@ -6,22 +6,27 @@
         {
             var totalDimensions = width * length * height;
             decimal cost = 0;
+            string description = string.Empty;
             switch (totalDimensions)
             {
                 case < 10:
                     cost = 3;
+                    description = "Small Parcel";
                     break;
                 case < 50:
                     cost = 8;
+                    description = "Medium Parcel";
                     break;
                 case < 100:
                     cost = 15;
+                    description = "Large Parcel";
                     break;
                 default:
                     cost = 25;
+                    description = "XL Parcel";
                     break;
             }
-            return new OrderCostResult(new[] { new OrderItem(cost) });
+            return new OrderCostResult(new[] { new OrderItem(cost, description) });
         }
     }
 }
--- CourierKata/OrderItem.cs
+++ CourierKata/OrderItem.cs
@@ -4,10 +4,12 @@ namespace CourierKata
     public class OrderItem
     {
         public decimal Cost { get; }
+        public string Description { get; }
 
-        public OrderItem(decimal cost)
+        public OrderItem(decimal cost, string description)
         {
             Cost = cost;
+            Description = description;
         }
     }
 }
