714bc563b7f167b044780461197329201f4743c9
jennysjottings <jenny@jennysjottings.co.uk>
2021-12-19 08:23:12 +0000

    RED: Should calculate cost for a single parcel

---
+++ CourierKataTests/OrderCostCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace CourierKataTests
+{
+    public class OrderCostCalculatorTests
+    {
+        [Test]
+        public void ShouldCalculateCostForASingleParcel()
+        {
+            var width = 1;
+            var length = 1;
+            var height = 1;
+
+            var calculator = new OrderCostCalculator();
+
+            var result = calculator.CalculateCost(width, length, height);
+
+            Assert.Equals(3.00, result.TotalCost);
+        }
+    }
+}
